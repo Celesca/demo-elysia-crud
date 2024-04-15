efcaf9a4fca5cf186b277e10e12f087fd5bd3311
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-15 16:05:19 +0700

    :+1: initial project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+type Book = {
+  id: number;
+  title: string;
+  price: number;
+}
+
+let bookList: Book[] = [
+  {"id": 1, "title": "Refactoring", "price": 100},
+  {"id": 2, "title": "Clean Code", "price": 200},
+]
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
